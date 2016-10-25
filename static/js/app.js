c3852775e5af0c815fae17c120503b77bed6acc9
philippe <bazard.philippe@gmail.com>
2016-10-25 16:42:01 +0200

    Look and feel - Add toolbar

--- static/js/app.js
+++ static/js/app.js
@@ -4,23 +4,12 @@
 
 app = angular.module('gwapitApp', ['ngSanitize']);
 
+/**
+ * Test
+ */
 app.controller('helloCtrl', function ($scope, $http) {
         $http.get('http://rest-service.guides.spring.io/greeting').then(function (response) {
             $scope.greeting = response.data;
             console.log($scope.greeting);
-        });
-    });
-
-
-app.controller('gmailCtrl', function ($scope, $http) {
-
-        req = 'https://www.googleapis.com/gmail/v1/users/'
-                + 'bazard.philippe@gmail.com'
-                + '/messages?key='
-                + 'ya29.Ci-GAzgyXr1D4LWA6wZq54hFtejHdESLOWrMJ1sTdCWhU3xdTspRBf2MhoEgPNlNCQ';
-
-        $http.get(req).then(function (response) {
-            $scope.res = response.data;
-            console.log($scope.res);
         });
     });
