bdceabe6bd072b791fd737fb156e7553937a4b23
philippe <bazard.philippe@gmail.com>
2016-10-28 07:37:52 +0200

    Add gmail controller

--- static/js/app.js
+++ static/js/app.js
@@ -4,12 +4,9 @@
 
 app = angular.module('gwapitApp', ['ngSanitize']);
 
-/**
- * Test
- */
-app.controller('helloCtrl', function ($scope, $http) {
-        $http.get('http://rest-service.guides.spring.io/greeting').then(function (response) {
-            $scope.greeting = response.data;
-            console.log($scope.greeting);
+app.controller('gmailCtrl', function ($scope, $http) {
+        $http.get('http://localhost:8000/test').then(function (response) {
+            $scope.records = response.data;
+            console.log($scope.records);
         });
     });
